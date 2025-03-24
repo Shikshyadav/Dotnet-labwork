e98592832826269e4fdb30aee4596916f96e2008
Shiksha Yadav <shikshyadav81@gmail.com>
2025-03-24 20:57:30 +0545

    2nd lab codes

---
+++ lab 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using Lab;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* 
+         *
+         Sum sum = new Sum();
+         sum.addition();
+        
+        Swap s1 = new Swap();
+        s1.swapping();
+
+        
+
+        GreatestuNm num1 = new GreatestNum();
+        num1.greatest();
+       
+        
+        EvenOdd e1 = new EvenOdd();
+        e1.checkevenodd();
+        
+
+        SumOfNum s1 = new SumOfNum();
+        s1.add();
+     
+
+        Factorial f1 = new Factorial();
+        f1.takefactorial();
+          
+
+
+        SumOfArray sumofarray = new SumOfArray();
+        sumofarray.addarray();
+       
+        LeapYear l1 = new LeapYear();
+        l1.checkleap();
+
+       
+
+        Prime prime = new Prime();
+        prime.checkprime();
+
+        */
+
+
+    }
+}
