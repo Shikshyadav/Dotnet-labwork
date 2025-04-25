2d35dbd0c94080407697c2774c0afc3fab3abcc6
Shiksha Yadav <shikshyadav81@gmail.com>
2025-04-25 09:06:27 +0545

    calculator

---
+++ lab 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ConsoleApp1;
+//using lab_3.sample;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MathOperation o1 = new MathOperation();
+        double doubleAddResult = o1.Add(5.4, 10.5);
+        Console.WriteLine("Result is  " + doubleAddResult);
+
+     
+        Console.ReadKey();
+
+      
+
+
+    }
+}
+
+
